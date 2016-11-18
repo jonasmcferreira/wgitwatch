7b8469b4234807f44bacfb18a7abc11884970c02
Jonas <jonas@fonix.com>
2016-11-18 15:23:48 +0000

    fixing the double quotes

--- gitwatch.bat
+++ gitwatch.bat
@@ -1,5 +1,5 @@
 cd C:\path\to\your\git\project
 git add --all
-git commit -m 'wgitwatch autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%'
+git commit -m "wgitwatch autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
 git push
 exit
